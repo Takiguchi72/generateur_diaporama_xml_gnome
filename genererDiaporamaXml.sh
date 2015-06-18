8872312e2b9929778e800f502ec90c05351364e5
Takiguchi <takiguchi@TakiDebian>
2015-06-18 19:26:39 +0200

    Prise en charge du dossier de destination du fichier xml via le 2ème paramètre

--- genererDiaporamaXml.sh
+++ genererDiaporamaXml.sh
@@ -8,7 +8,7 @@
 #####################
 
 # Fonction permettant d'écrire le paragraphe correspondant à une image dans le fichier xml
-# @param Le nom de l'image à traiter [String]
+# @param $1 - Le nom de l'image à traiter [String]
 ajouterImage()
 {
 	
@@ -19,10 +19,10 @@ ajouterImage()
 		exit 1
 	else
 		echo "Ajout de l'image '$1' au fichier xml..."
-		echo "	<static>" >> $xmlFileName
-		echo "		<duration>$duree</duration>" >> $xmlFileName
-		echo "		<file>$folderPath$1</file>" >> $xmlFileName
-		echo "	</static>" >> $xmlFileName
+		echo "	<static>" >> $diaporama
+		echo "		<duration>$duree</duration>" >> $diaporama
+		echo "		<file>$dossierDImages/$1</file>" >> $diaporama
+		echo "	</static>" >> $diaporama
 		echo "Image ajoutée."
 		echo " "
 	fi
@@ -33,42 +33,51 @@ ajouterImage()
 #########################################
 
 # Liste des variables
-# $1 <=> Chemin du dossier contenant les images
-folderPath=$1
-# $2 <=> Chemin du fichier xml
-xmlFileName="fond_ecran_dynamique.xml"
+dossierDImages=$1									# $1 <=> Chemin du dossier contenant les images
+dossierDImages=${dossierDImages%/}		# On supprime le "/" à la fin de la chaine, s'il y en a un
+dossierDuDiaporama=$2								# $2 <=> Chemin du diamopara
+nomDuDiaporama="fond_ecran_dynamique.xml"
 duree="60.0"
 
-# Si l'utilisateur n'a pas spécifié le chemin du fichier xml à générer, on le placera dans le dossier personnel
-#if [ -z $2 ]
-#then
-	xmlFilePath=$(cd && pwd)/$xmlFileName
-#else
-#	xmlFilePath=$2/$xmlFileName
-#fi
-
-contenuDossier=$(ls $1)
-nombreFichiers=$(ls $1 | wc -l) 
+nombreFichiers=$(ls $dossierDImages | wc -l) 
 # Si le contenu du dossier qu'a choisit l'utilisateur est vide, on affiche une erreur et on arrête le script
 if [ $nombreFichiers -eq 0 ]
 then
-	echo "Il n'y a rien dans le dossier '$1'..."
+	echo "Il n'y a rien dans le dossier '$dossierDImages'..."
 	echo "Génération du fichier xml annulée..."
 	exit 1
 fi
 
-echo "<background>" > $xmlFileName
+# On vérifie si l'utilisateur a spécifié où il souhaitait que le diaporama soit généré :
+# Si il ne l'a pas spécifié,
+if [ -z $dossierDuDiaporama ]
+then
+	# On va créer le diaporama à la racine de son dossier personnel (/home/[Utilisateur]/fond_ecran_dynamique.xml)
+	diaporama=$(cd && pwd)/$nomDuDiaporama
+else
+	# Sinon on le créer là où l'a précisé
+	diaporama=${dossierDuDiaporama%/}/$nomDuDiaporama	# "${chaine%/}" permet de supprimer le "/" à la fin de la chaine, s'il y en a un...
+fi
 
-# Pour chaque image dans le dossier qu'a choisit l'utilisateur, on va ajouter un bloc dans le fichier xml
-for fichier in $(ls $1)
+echo "<background>" > $diaporama
+
+# Si la commande précédente a retourné autre chose que "0", c'est qu'il est impossible de générer le fichier xml, donc on arrête le script
+if [ $? -ne 0 ]
+then
+	echo "Impossible de créer/modifier le fichier xml !"
+	exit 1
+fi
+
+# Pour chaque fichier dans le dossier qu'a choisit l'utilisateur,
+for fichier in $(ls $dossierDImages)
 do
-	# Si l'extension du fichier est "jpg" ou "png", on ajoute l'image au diaporama
+	# Si le fichier est une image "jpg" ou "png", on ajoute l'image au diaporama
 	if [ ${fichier##*.} = "jpg" -o ${fichier##*.} = "png" ]
 	then
 		ajouterImage $fichier
 	fi
 done
-echo "</background>" >> $xmlFileName
-echo " "
+
+echo "</background>" >> $diaporama
 echo "Votre diaporama a été enregistré ici :"
-echo $xmlFilePath
+echo $diaporama
